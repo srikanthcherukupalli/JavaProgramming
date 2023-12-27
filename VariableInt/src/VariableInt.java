64279ccbb3b4263458396e63a0eb4fe8a5dcd736
SRIKANTH CHERUKUPALLI <srikanth.venkatachary@gmail.com>
2023-12-27 17:36:36 +0530

    added extra code in VariableInt

--- VariableInt/src/VariableInt.java
+++ VariableInt/src/VariableInt.java
@@ -2,5 +2,7 @@ public class VariableInt {
     public static void main(String[] args) {
         int  age=18;
         System.out.println(age);
+        age = 24;
+        System.out.println("now age is:" + age);
     }
 }
