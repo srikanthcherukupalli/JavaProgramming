7d9ecc66cd5721d6c7e34c35f96683799e94061c
SRIKANTH CHERUKUPALLI <srikanth.venkatachary@gmail.com>
2023-12-28 18:14:08 +0530

    added src for utiltiy

---
+++ Utility/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        System.out.println("Please type your name!");
+        Scanner myName = new Scanner(System.in);
+        String userName = myName.nextLine();
+        System.out.println("Hello " + userName + "!!! Advance Happy New Year!!!");
+    }
+}
